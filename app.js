06cfa5609c8be95f050639a7f28e184cf35c01db
bongomin <candanobongomindaniel@gmail.com>
2019-08-09 16:40:45 +0300

    feat(GUI Design) : designed the home page GUI

--- app.js
+++ app.js
@@ -3,15 +3,38 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var hbs = require('express-handlebars');
+
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var homeRouter = require('./routes/home');
+var aboutRouter = require('./routes/about');
+var useRouter = require('./routes/use')
 
 var app = express();
 
-// view engine setup
+// date middleware
+app.use( (req,res,next) => {
+  console.log(Date.now());
+  req.name = 'danny Daniels';
+  next();
+});
+
+// view engine setup  // handle bars templating engine middleware
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
+
+// middleware for locating the partial file
+app.engine('hbs', hbs({
+  extname: 'hbs', 
+  defaultLayout: 'layout', 
+  layoutsDir: path.join(__dirname, 'views'),
+  partialsDir  : [
+      //  path to your partials
+      path.join(__dirname, 'views/partials'),
+  ]
+}));
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -19,8 +42,11 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
+app.use('/index', indexRouter);
 app.use('/users', usersRouter);
+app.use('/', homeRouter);
+app.use('/use',useRouter);
+app.use('/about',aboutRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
