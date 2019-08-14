952861fd2965e5cf84f53078666f3aa786fe6e1c
bongomin <candanobongomindaniel@gmail.com>
2019-08-14 13:03:40 +0300

    feat(Adding Flash Messages) : installed modules that support flash messages
    - installed connect-flash modules for delclaring flash global variables
    
    - installed express seessions

--- models/Password.js
+++ models/Password.js
@@ -4,22 +4,22 @@ const Schema = mongoose.Schema;
 // create a schema for passwords
 
 const PasswordSchema = new Schema({
-    systemName : {
-        type : String,
-        required : true
+    systemName: {
+        type: String,
+        required: true
     },
-    userName :{
-        type :String,
-        required : true
+    userName: {
+        type: String,
+        required: true
     },
-    passWord :{
-        type:String,
-        required :true
+    passWord: {
+        type: String,
+        required: true
     },
     date: {
         type: Date,
         default: Date.now
-      }
+    }
 
 });
 
