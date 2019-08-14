b18d2cee846074241f3b103ee2f1bef1a2da64c8
bongomin <candanobongomindaniel@gmail.com>
2019-08-14 18:02:45 +0300

    feat(register and login) : register and login end points
    - use of  bcrype module to encrypt passwords
    
    - validation in the register router
    
    - validating the passwords length

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,43 @@
 var express = require('express');
 var router = express.Router();
+var mongoose = require('mongoose');
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
-});
+// Users login Route
+router.get('/login' , (req,res) => {
+  res.render('users/login');
+})
+
+// Users Register Route
+router.get('/register' , (req,res) => {
+ res.render('users/register');
+})
+
+// Register form post
+router.post('/register',(req,res) => {
+  let errors = []
+  if(req.body.password != req.body.password2){
+    errors.push({text :'Password do not Match Buddy !!'})
+  }
+
+  if(req.body.password.length < 4){
+    errors.push({text :'Password must be atleast 4 characters'})
+  }
+  if(errors.length > 0){
+    res.render('users/register' , {
+      errors : errors,
+      firstName : req.body.firstName,
+      secondName : req.body.secondName,
+      email: req.body.email,
+      password : req.body.password,
+      password2 :req.body.password2
+
+    });
+  }else{
+    res.send('passed');
+
+  }
+})
+
 
 module.exports = router;
+
