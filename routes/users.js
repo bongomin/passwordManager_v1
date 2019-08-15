99318c8791aaaa8f671e3321c8517597d431d00e
bongomin <candanobongomindaniel@gmail.com>
2019-08-15 16:25:39 +0300

    feat(used local startegy module and passport) : created a config folder and a passprt file to handle.
    - handle Localstartegy
    
    - use of passport

--- routes/users.js
+++ routes/users.js
@@ -10,53 +10,63 @@ require('../models/users');
 var User = mongoose.model('users');
 
 // Users login Route
-router.get('/login' , (req,res) => {
+router.get('/login', (req, res) => {
   res.render('users/login');
 })
 
 // Users Register Route
-router.get('/register' , (req,res) => {
- res.render('users/register');
+router.get('/register', (req, res) => {
+  res.render('users/register');
 })
 
+// Login Form Post
+
+router.post('/login', (req, res, next) => {
+  passport.authenticate('local', {
+    successRedirect: '/passwords',
+    failureRedirect: '/users/login',
+    failureFlash: true
+  })(req, res, next);
+});
+
 // Register form post
-router.post('/register',(req,res) => {
+router.post('/register', (req, res) => {
   let errors = []
-  if(req.body.password != req.body.password2){
-    errors.push({text :'Password do not Match Buddy !!'})
+  if (req.body.password != req.body.password2) {
+    errors.push({ text: 'Password do not Match Buddy !!' })
   }
 
-  if(req.body.password.length < 4){
-    errors.push({text :'Password must be atleast 4 characters'})
+  if (req.body.password.length < 4) {
+    errors.push({ text: 'Password must be atleast 4 characters' })
   }
-  if(errors.length > 0){
-    res.render('users/register' , {
-      errors : errors,
-      firstName : req.body.firstName,
-      secondName : req.body.secondName,
+  if (errors.length > 0) {
+    res.render('users/register', {
+      errors: errors,
+      firstName: req.body.firstName,
+      secondName: req.body.secondName,
       email: req.body.email,
-      password : req.body.password,
-      password2 : req.body.password2
+      password: req.body.password,
+      password2: req.body.password2
 
     });
-  }else {
-    User.findOne({email: req.body.email})
+  } else {
+    User.findOne({ email: req.body.email })
       .then(user => {
-        if(user){
+        if (user) {
           req.flash('error_msg', 'Email already regsitered');
           res.redirect('/users/register');
         } else {
           const newUser = new User({
-            firstName : req.body.firstName,
-            secondName : req.body.secondName,
+            firstName: req.body.firstName,
+            secondName: req.body.secondName,
             email: req.body.email,
-            password : req.body.password,
-            password2 : req.body.password2
+            password: req.body.password,
+            password2: req.body.password2
           });
-          
+
           bcrypt.genSalt(10, (err, salt) => {
             bcrypt.hash(newUser.password, salt, (err, hash) => {
-              if(err) throw err;
+              if (err) throw err;
               newUser.password = hash;
               newUser.save()
                 .then(user => {
@@ -70,9 +80,9 @@ router.post('/register',(req,res) => {
             });
           });
 
-      }
-    });
-   
+        }
+      });
+
   }
 });
 
