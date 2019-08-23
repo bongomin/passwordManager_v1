4247dfda4d816f4b5f9e942606cc9bdebae73044
bongomin <candanobongomindaniel@gmail.com>
2019-08-23 10:13:52 +0300

    feat(configuring db): configuring cloud db atlas to the application

--- config/database.js
+++ config/database.js
@@ -1,5 +1,5 @@
 if(process.env.NODE_ENV === 'production'){
-    module.exports ={ mongoURI: 'mongodb+srv://password-manager:P@55w0rd55@passwordmanager1-xfw49.mongodb.net/test?retryWrites=true&w=majority'
+    module.exports ={ mongoURI: 'mongodb+srv://danny:P@55w0rd55@passwordmanager1-xfw49.mongodb.net/test?retryWrites=true&w=majority'
     
     }
 
